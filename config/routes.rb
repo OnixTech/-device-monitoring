31ae1f922a5583b1d861e9883e9b6346357ab809
Luciano Petillo <luciano.prnl@gmail.com>
2022-07-07 14:23:35 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
-  resources :technicians only:[:create,:show,:edit,:destroy]
-  resources :devices only:[:create,:show,:edit,:destroy]
+  resources :technicians, only:[:create,:show,:edit,:destroy]
+  resources :devices, only:[:create,:show,:edit,:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
